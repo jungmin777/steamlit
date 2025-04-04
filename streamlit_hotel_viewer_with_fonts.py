106ad361931f843995a762a480aceca8592cf4da
jungmin777 <lkarawl97@gmail.com>
2025-04-04 22:35:08 +0900

    Update streamlit_hotel_viewer_with_fonts.py

--- streamlit_hotel_viewer_with_fonts.py
+++ streamlit_hotel_viewer_with_fonts.py
@@ -50,7 +50,7 @@ for idx, row in enumerate(top_hotels.itertuples(), 1):
 #     m = folium.Map(
 #         location=[center_lat, center_lon], 
 #         zoom_start=zoom_start, 
-#         tiles="OpenStreetMap"
+#         tiles='CartoDB positron'
 #     )
     
 #     if len(dataframe) > 1:
@@ -105,7 +105,7 @@ def create_google_map(dataframe, zoom_start=12):
     m = folium.Map(
         location=[center_lat, center_lon], 
         zoom_start=zoom_start, 
-        tiles="OpenStreetMap"
+        tiles='CartoDB positron'
     )
     
     if len(dataframe) > 1:
